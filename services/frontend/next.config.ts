f6f6e59c0e0dece587bda53a403d514a46d06c36
thanhphan20 <thanhphan20@gmail.com>
2025-12-03 15:17:26 +0700

    feat: implement frontend admin dashboard ui template use shadcn

--- services/frontend/next.config.ts
+++ services/frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/default",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
